0b3b0251adb387fa56878e526d287698878817e3
Grigoryev Denis (DG5) <denis.ru200@yandex.ru>
2017-06-06 16:01:13 +0300

    DERMO CODE

--- T05DET/T05DET.C
+++ T05DET/T05DET.C
@@ -5,18 +5,27 @@
  */
 
 #include <stdio.h>
-#define max 15
+#include <math.h>
+#define Max 15
 
 #pragma warning (disable: 4244)
 
-int p[max], parity, n;
-double a[max][max], Det;
+int p[Max], parity, n, sign = 1;
+double a[Max][Max], Det = 566, MAX_i, MAX_j, MAX;
 
-/*
-void Max( void )
+void CountMAX( void )
 {
-  for
-} */
+  int i, j;
+  MAX = a[0][0];
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; i++)
+      if (MAX > a[i][j])
+      {  
+        MAX = fabs(a[i][j]);
+        MAX_i = i;
+        MAX_j = j;
+      }
+} 
 
 void Swap( int *A, int *B )
 {
@@ -24,6 +33,43 @@ void Swap( int *A, int *B )
 
   *A = *B;
   *B = tmp;
+}
+void MatrixSwapStrech( double MAX_i, double MAX_j )
+{
+  double tmp;
+  int g, i, j;
+  
+   for (i = MAX_i, j = MAX_j, g = 1; i < n; i++)
+   {
+     tmp = a[i][j];
+     a[i][j] = a[g][j];
+     a[g][j] = a[i][j];
+   }
+}
+void MatrixSwapTowers( double MAX_i, double MAX_j )
+{
+  double tmp;
+  int g, i, j;
+
+   for (j = MAX_j, i = MAX_i, g = 0; j < n; j++)
+   {
+     tmp = a[i][j];
+     a[i][j] = a[i][g];
+     a[i][g] = a[i][j];
+   }
+}
+void Gaus( void )
+{
+  int i, j;
+
+  for (i = 1; i < n; i++)
+    for (j = 1; j < n; i++)
+      a[i][j] = a[i][j] - a[0][0] * a[i][j] / a[0][0];
+  /*counting Determination */
+  for (i = 0, j = 0; i < n; i++, j++)
+    Det *= a[i][j];
+  Det *= sign;
+  
 }
 
 void MatrixLoad( char* FileName )
@@ -34,8 +80,8 @@ void MatrixLoad( char* FileName )
  if ((f = fopen("a.txt", "r")) == NULL)
    return;
  fscanf( f, "%d", &n);
- if (n > max)
-   n = max;
+ if (n > MAX)
+   n = MAX;
  for (i = 0; i < n; i++)
    for(j = 0; j < n; j++)
      fscanf(f,"%lf", &a[i][j]);
@@ -47,14 +93,9 @@ void PrintDet( void )
    FILE *f;
  
    f = fopen("OUTPUT.TXT", "a");
-   if (f == NULL)
-     return;
-   fprintf(f, "Det = %lf \n", Det);
+   fprintf(f, "NEWDet = %lf \n", Det);
    fclose(f);
  }
-
-
-
 void Go( int pos )
 {
   int i, Saveparity, tmp;
@@ -88,12 +129,20 @@ void Go( int pos )
 
 void main( void )
 {
-  int i;
-
   MatrixLoad("A.txt");
-  for (i = 0; i < n; i++)
-    p[i] = i;
-  Go(0);
+  CountMAX();
+  if (MAX_i != 0)
+  {
+    MatrixSwapStrech( MAX_i, MAX_j );
+    sign = -sign;
+  }
+   if (MAX_j != 0)
+   {
+     MatrixSwapTowers( MAX_i, MAX_j );
+     sign = -sign;
+   }
+   /* Go(0);*/
+  Gaus();
   PrintDet();
 }
 
