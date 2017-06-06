d5e44a3bde88aae9c6eb1c289590eba49579e4f5
Grigoryev Denis (DG5) <denis.ru200@yandex.ru>
2017-06-06 12:59:43 +0300

    COMENTS

--- T05DET/T05DET.C
+++ T05DET/T05DET.C
@@ -1,13 +1,22 @@
 /* FILE NAME: T05PERM.C
  * PROGRAMMER: DG5
- * DATE: 01.06.2017
- * PURPOSE: Deturmined of matrix.
+ * DATE: 06.06.2017
+ * PURPOSE: Deturmination of matrix.
  */
 
 #include <stdio.h>
 #define max 15
 
+#pragma warning (disable: 4244)
+
 int p[max], parity, n;
+double a[max][max], Det;
+
+/*
+void Max( void )
+{
+  for
+} */
 
 void Swap( int *A, int *B )
 {
@@ -16,49 +25,86 @@ void Swap( int *A, int *B )
   *A = *B;
   *B = tmp;
 }
-void PrintPerm( void )
 
-{ 
-  int i;
-  FILE *f;
-
-  f = fopen("Perm.TXT","a");
-  if (f == NULL)
-    return;
-  for (i = 0; i < n; i++)
-    fprintf(f, "%i", p[i]);
-  fprintf(f,"-%s\n", parity ? "odd" : "even");
-  fclose(f);
-}
-
-void Go( int Pos )
+void MatrixLoad( char* FileName )
 {
-  int i;
+ int i, j;
+ FILE *f;
 
-  if (Pos >= n)
+ if ((f = fopen("a.txt", "r")) == NULL)
+   return;
+ fscanf( f, "%d", &n);
+ if (n > max)
+   n = max;
+ for (i = 0; i < n; i++)
+   for(j = 0; j < n; j++)
+     fscanf(f,"%lf", &a[i][j]);
+ fclose(f);
+}
+
+void PrintDet( void )
+ {
+   FILE *f;
+ 
+   f = fopen("OUTPUT.TXT", "a");
+   if (f == NULL)
+     return;
+   fprintf(f, "Det = %lf \n", Det);
+   fclose(f);
+ }
+
+
+
+void Go( int pos )
+{
+  int i, Saveparity, tmp;
+  double  mtmp = 1;
+                                          
+  if (pos >= n)
   {
-    PrintPerm();
+    for (i = 0; i < n; i++)
+      mtmp *= a[i][p[i]];
+    if (parity)
+      Det -= mtmp;
+    else
+      Det += mtmp;
     return;
   }
-  for (i = Pos; i < n; i++)
-  {  
-    Swap(&p[Pos], &p[i]);
-    if (Pos != i)
-      parity = !parity;
-    Go(Pos + 1);
-    Swap(&p[Pos], &p[i]);
-    if (Pos != i)
-      parity = !parity;
+  Go(pos + 1);
+  Saveparity = parity;
+  for (i = pos + 1; i < n; i++)
+  {
+    Swap(&p[pos], &p[i]);
+    parity = !parity;
+    Go(pos + 1);
   }
+  tmp = p[pos];
+  for (i  = pos + 1; i < n; i++)
+    p[i - 1] = p[i];
+  p[n - 1] = tmp;
+  parity = Saveparity;
 }
+
 
 void main( void )
 {
   int i;
 
-  n = 3;
-  parity = 0;
+  MatrixLoad("A.txt");
   for (i = 0; i < n; i++)
-    p[i] = i + 1;
+    p[i] = i;
   Go(0);
+  PrintDet();
 }
+
+
+
+
+
+
+
+ 
+
+
+
+
