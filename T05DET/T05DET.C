743cf05ebb01b9836b75dbb9b97127228f69c4d0
Grigoryev Denis (DG5) <denis.ru200@yandex.ru>
2017-06-07 14:04:51 +0300

    1.2.3.8.489084014841.5

--- T05DET/T05DET.C
+++ T05DET/T05DET.C
@@ -11,140 +11,95 @@
 #pragma warning (disable: 4244)
 
 int p[Max], parity, n, sign = 1;
-double a[Max][Max], Det = 566, MAX_i, MAX_j, MAX;
+double a[Max][Max], Det = 1;
 
-void CountMAX( void )
+void Swap( double *A, double *B )
 {
-  int i, j;
-  MAX = a[0][0];
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; i++)
-      if (MAX > a[i][j])
-      {  
-        MAX = fabs(a[i][j]);
-        MAX_i = i;
-        MAX_j = j;
-      }
-} 
-
-void Swap( int *A, int *B )
-{
-  int tmp = *A;
+  double tmp = *A;
 
   *A = *B;
   *B = tmp;
-}
-void MatrixSwapStrech( double MAX_i, double MAX_j )
-{
-  double tmp;
-  int g, i, j;
-  
-   for (i = MAX_i, j = MAX_j, g = 1; i < n; i++)
-   {
-     tmp = a[i][j];
-     a[i][j] = a[g][j];
-     a[g][j] = a[i][j];
-   }
-}
-void MatrixSwapTowers( double MAX_i, double MAX_j )
-{
-  double tmp;
-  int g, i, j;
+}/* End of "Swap" function */
 
-   for (j = MAX_j, i = MAX_i, g = 0; j < n; j++)
-   {
-     tmp = a[i][j];
-     a[i][j] = a[i][g];
-     a[i][g] = a[i][j];
-   }
-}
-void Gaus( void )
+void Gauss( void )
 {
-  int i, j;
+  int i, j, k, x, y, coef;
 
-  for (i = 1; i < n; i++)
-    for (j = 1; j < n; i++)
-      a[i][j] = a[i][j] - a[0][0] * a[i][j] / a[0][0];
-  /*counting Determination */
-  for (i = 0, j = 0; i < n; i++, j++)
-    Det *= a[i][j];
-  Det *= sign;
+  for (i = 0; i < n; i++)
+  { 
+    /* looking for maximum matrix element in every row and colum */
+    k = i;
+    j = k;
+    for (y = i; y < n; y++)
+      for(x = i; x < n; x++)
+        if (fabs(a[y][x]) > fabs(a[j][k]))
+        {  
+          k = x;
+          j = y;
+        }
+     if (a[j][k] == 0)
+     {
+       Det = 0;
+       return ;
+     }
+     if (j != i) /* Swap j row and i row (elements[i..n-1]*/
+     {
+       for (x = i; x < n; x++);
+         Swap( &a[i][x], &a[j][x]);
+       Det = - Det;
+     }
+       
+     if (k != i) /* Swap j row and i row (elements[i..n-1]*/
+     {
+       for (x = i; x < n; x++)
+         Swap(&a[i][x], &a[j][x]);
+       Det = - Det;
+     }
+     /* Subtrack from every row [i+1...n-1]*/
+     for (j = i + 1; j < n; j++)
+     {
+       coef = a[j][i] / a[i][i];
+       for (k = i; k < n; k++)
+         a[j][k] -= a[i][k] * coef;
+     }
+  }/* End of FOR (i = 0; i < n; i++) */
   
-}
+  /* Counting determination*/
+  for (i = 0; i < n; i++)
+    Det *= a[i][i];
+}/* End of "Gauss" function */
 
 void MatrixLoad( char* FileName )
 {
  int i, j;
  FILE *f;
 
- if ((f = fopen("a.txt", "r")) == NULL)
+ if ((f = fopen("Matrix.txt", "r")) == NULL)
    return;
- fscanf( f, "%d", &n);
- if (n > MAX)
-   n = MAX;
+ fscanf(f, "%d", &n);
+ if (n > Max)
+   n = Max;
  for (i = 0; i < n; i++)
    for(j = 0; j < n; j++)
      fscanf(f,"%lf", &a[i][j]);
  fclose(f);
-}
+}/* End of "MatrixLoad" function */
 
 void PrintDet( void )
  {
    FILE *f;
  
-   f = fopen("OUTPUT.TXT", "a");
-   fprintf(f, "NEWDet = %lf \n", Det);
+   f = fopen("Answer.TXT", "a");
+   fprintf(f, "Det = %lf \n", Det);
    fclose(f);
- }
-void Go( int pos )
-{
-  int i, Saveparity, tmp;
-  double  mtmp = 1;
-                                          
-  if (pos >= n)
-  {
-    for (i = 0; i < n; i++)
-      mtmp *= a[i][p[i]];
-    if (parity)
-      Det -= mtmp;
-    else
-      Det += mtmp;
-    return;
-  }
-  Go(pos + 1);
-  Saveparity = parity;
-  for (i = pos + 1; i < n; i++)
-  {
-    Swap(&p[pos], &p[i]);
-    parity = !parity;
-    Go(pos + 1);
-  }
-  tmp = p[pos];
-  for (i  = pos + 1; i < n; i++)
-    p[i - 1] = p[i];
-  p[n - 1] = tmp;
-  parity = Saveparity;
-}
-
+ }/* End of "PrintDet" function */
 
 void main( void )
 {
-  MatrixLoad("A.txt");
-  CountMAX();
-  if (MAX_i != 0)
-  {
-    MatrixSwapStrech( MAX_i, MAX_j );
-    sign = -sign;
-  }
-   if (MAX_j != 0)
-   {
-     MatrixSwapTowers( MAX_i, MAX_j );
-     sign = -sign;
-   }
-   /* Go(0);*/
-  Gaus();
+  MatrixLoad("Matrix.txt");
+  Gauss();
   PrintDet();
-}
+} /* End of "main" function */
 
 
 
